bd157b2ced5494dee9c13868bf3124682e090184
Anderson Soriano <andersori7@gmail.com>
2016-11-11 16:10:14 -0200

    Evento alterado

--- Pradoc2/src/java/modelo/Evento.java
+++ Pradoc2/src/java/modelo/Evento.java
@@ -54,81 +54,112 @@ public class Evento {
         this.areaConhecimento.add(areaConhecimento);
     }
 
-    public String getInicioSubmicao() {   //V  
-        String hora = Integer.toString(inicioSubmicao.get(Calendar.HOUR_OF_DAY));
-        String minuto = Integer.toString(inicioSubmicao.get(Calendar.MINUTE));
-        String segundo = Integer.toString(inicioSubmicao.get(Calendar.SECOND));
-        String dia = Integer.toString(inicioSubmicao.get(Calendar.DAY_OF_MONTH));
-        String mes = Integer.toString(inicioSubmicao.get(Calendar.MONTH));
-        String ano = Integer.toString(inicioSubmicao.get(Calendar.YEAR));
-        
-        String DInicio = "Hora: " + hora.concat(":" + minuto + ":" + segundo + ". Data: " + dia + "/" + mes + "/" + ano + ".");
-        
-        return DInicio;
+    public Calendar getInicioSubmicao() {   //V  
+        return inicioSubmicao;
     }
 
-    public void setInicioSubmicao(Calendar inicioSubmicao) {    //V
-        this.inicioSubmicao = inicioSubmicao;
-    }
-
-    public String getFimSubmicao() {    //V
-        String hora = Integer.toString(inicioSubmicao.get(Calendar.HOUR_OF_DAY));
-        String minuto = Integer.toString(inicioSubmicao.get(Calendar.MINUTE));
-        String segundo = Integer.toString(inicioSubmicao.get(Calendar.SECOND));
-        String dia = Integer.toString(inicioSubmicao.get(Calendar.DAY_OF_MONTH));
-        String mes = Integer.toString(inicioSubmicao.get(Calendar.MONTH));
-        String ano = Integer.toString(inicioSubmicao.get(Calendar.YEAR));
+    public void setInicioSubmicao(int hora, int minuto, int segundo, int dia, int mes, int ano) {
         
-        String DFim = "Hora: " + hora.concat(":" + minuto + ":" + segundo + ". Data: " + dia + "/" + mes + "/" + ano + ".");
+        //Verificar os erro nessas passagem de parametros
         
-        return DFim;
+        this.inicioSubmicao.set(Calendar.HOUR_OF_DAY, hora);
+        this.inicioSubmicao.set(Calendar.MINUTE, minuto);
+        this.inicioSubmicao.set(Calendar.SECOND, segundo);
+        this.inicioSubmicao.set(Calendar.DAY_OF_MONTH, dia);
+        this.inicioSubmicao.set(Calendar.MONTH, mes);
+        this.inicioSubmicao.set(Calendar.YEAR, ano);
     }
 
-    public void setFimSubmicao(Calendar fimSubmicao) {
-        this.fimSubmicao = fimSubmicao;
+    public Calendar getFimSubmicao() {    //V
+        return fimAvaliacao;
     }
 
-    public Calendar getInicioAvaliacao() {
+    public void setFimSubmicao(int hora, int minuto, int segundo, int dia, int mes, int ano) {
+        
+        //Verificar os erro nessas passagem de parametros
+        
+        this.fimSubmicao.set(Calendar.HOUR_OF_DAY, hora);
+        this.fimSubmicao.set(Calendar.MINUTE, minuto);
+        this.fimSubmicao.set(Calendar.SECOND, segundo);
+        this.fimSubmicao.set(Calendar.DAY_OF_MONTH, dia);
+        this.fimSubmicao.set(Calendar.MONTH, mes);
+        this.fimSubmicao.set(Calendar.YEAR, ano);
+        
+    }
+
+    public Calendar getInicioAvaliacao() {  //OK
         return inicioAvaliacao;
     }
 
-    public void setInicioAvaliacao(Calendar inicioAvaliacao) {
-        this.inicioAvaliacao = inicioAvaliacao;
+    public void setInicioAvaliacao(int hora, int minuto, int segundo, int dia, int mes, int ano) {
+        
+        //Verificar os erro nessas passagem de parametros
+        
+        this.inicioAvaliacao.set(Calendar.HOUR_OF_DAY, hora);
+        this.inicioAvaliacao.set(Calendar.MINUTE, minuto);
+        this.inicioAvaliacao.set(Calendar.SECOND, segundo);
+        this.inicioAvaliacao.set(Calendar.DAY_OF_MONTH, dia);
+        this.inicioAvaliacao.set(Calendar.MONTH, mes);
+        this.inicioAvaliacao.set(Calendar.YEAR, ano);
     }
 
     public Calendar getFimAvaliacao() {
         return fimAvaliacao;
     }
 
-    public void setFimAvaliacao(Calendar fimAvaliacao) {
-        this.fimAvaliacao = fimAvaliacao;
+    public void setFimAvaliacao(int hora, int minuto, int segundo, int dia, int mes, int ano) {
+        
+        //Verificar os erro nessas passagem de parametros
+        
+        this.fimAvaliacao.set(Calendar.HOUR_OF_DAY, hora);
+        this.fimAvaliacao.set(Calendar.MINUTE, minuto);
+        this.fimAvaliacao.set(Calendar.SECOND, segundo);
+        this.fimAvaliacao.set(Calendar.DAY_OF_MONTH, dia);
+        this.fimAvaliacao.set(Calendar.MONTH, mes);
+        this.fimAvaliacao.set(Calendar.YEAR, ano);
     }
 
     public Calendar getInicioRecurso() {
         return inicioRecurso;
     }
 
-    public void setInicioRecurso(Calendar inicioRecurso) {
-        this.inicioRecurso = inicioRecurso;
+    public void setInicioRecurso(int hora, int minuto, int segundo, int dia, int mes, int ano) {
+        
+        //Verificar os erro nessas passagem de parametros
+        
+        this.inicioRecurso.set(Calendar.HOUR_OF_DAY, hora);
+        this.inicioRecurso.set(Calendar.MINUTE, minuto);
+        this.inicioRecurso.set(Calendar.SECOND, segundo);
+        this.inicioRecurso.set(Calendar.DAY_OF_MONTH, dia);
+        this.inicioRecurso.set(Calendar.MONTH, mes);
+        this.inicioRecurso.set(Calendar.YEAR, ano);
     }
 
     public Calendar getFimRecurso() {
         return fimRecurso;
     }
 
-    public void setFimRecurso(Calendar fimRecurso) {
-        this.fimRecurso = fimRecurso;
+    public void setFimRecurso(int hora, int minuto, int segundo, int dia, int mes, int ano) {
+        
+        //Verificar os erro nessas passagem de parametros
+        
+        this.fimRecurso.set(Calendar.HOUR_OF_DAY, hora);
+        this.fimRecurso.set(Calendar.MINUTE, minuto);
+        this.fimRecurso.set(Calendar.SECOND, segundo);
+        this.fimRecurso.set(Calendar.DAY_OF_MONTH, dia);
+        this.fimRecurso.set(Calendar.MONTH, mes);
+        this.fimRecurso.set(Calendar.YEAR, ano);
     }
 
     public String getInstituicao() {
         return instituicao;
     }
 
-    public void setInstituicao(String instituicao) {
+    public void setInstituicao(String instituicao) {    //OK?
         this.instituicao = instituicao;
     }
 
-    public Organizador getOrganizador() {
+    public Organizador getOrganizador() {   //OK?
         return organizador;
     }
 
