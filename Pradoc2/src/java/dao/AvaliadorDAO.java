71ec5e09803e1e37c5191fc7263dc5b5c61dafea
Anderson Soriano <andersori7@gmail.com>
2016-11-27 17:52:43 -0200

    Não terminei

---
+++ Pradoc2/src/java/dao/AvaliadorDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Anderson
+ */
+public class AvaliadorDAO {
+    public void insert(){
+        Connection con = ConnectionFactory.getConnection();
+    }
+    
+    
+}
