b07d0457f02be5fa541f16634a1952d81cee83b2
Anderson Soriano <andersori7@gmail.com>
2016-12-11 13:41:06 -0200

    Banco Atualizado, Evento estruturado

--- Pradoc/src/java/modelo/Evento.java
+++ Pradoc/src/java/modelo/Evento.java
@@ -5,11 +5,9 @@
  */
 package modelo;
 
-import br.com.pradoc.iterators.ParticipacaoList;
+import br.com.pradoc.iterators.CompetenciaList;
 import br.com.pradoc.iterators.UsuarioList;
-import java.sql.Date;
-import java.sql.Time;
-import java.util.List;
+import java.util.Calendar;
 
 /**
  *
@@ -18,52 +16,20 @@ import java.util.List;
 public class Evento {
     private int id;
     private String nome;
-    private List<String> areaConhecimento;// No banco não tem essa Lista 29/11
-    private float notaAprovacao;
     private String descricao;
-    private Date inicioSubmissao;
-    private Date fimSubmissao;
-    private Date inicioAvaliacao;
-    private Date fimAvaliacao;
-    private Date inicioRecurso;
-    private Date fimRecurso;
-    private Date resultadoRecurso;
-    private Time horaInicioSubmissao;
-    private Time horaFimSubmissao;
-    private Time horaInicioAvaliacao;
-    private Time horaFimAvaliacao;
-    private Time horaInicioRecurso;
-    private Time horaFimRecurso;
     private String instituicao;
+    private double notaDeAprovacao;
+    private Calendar inicioSubimicao;
+    private Calendar fimSubmicao;
+    private Calendar inicioAvaliacao;
+    private Calendar fimAvaliacao;
+    private Calendar inicioRecurso;
+    private Calendar fimRecurso;
     private Usuario organizador;
     private UsuarioList avaliadores;
-    private ParticipacaoList participacoes;
     private Arquivo modeloDocumento;
+    private CompetenciaList competencias;
 
-    public Date getResultadoRecurso() {
-        return resultadoRecurso;
-    }
-
-    public void setResultadoRecurso(Date resultadoRecurso) {
-        this.resultadoRecurso = resultadoRecurso;
-    }
-    
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-        
-    public float getNotaAprovacao() {
-        return notaAprovacao;
-    }
-
-    public void setNotaAprovacao(float notaAprovacao) {
-        this.notaAprovacao = notaAprovacao;
-    }
-    
     public int getId() {
         return id;
     }
@@ -72,143 +38,102 @@ public class Evento {
         this.id = id;
     }
 
-    public Date getInicioAvaliacao() {
-        return inicioAvaliacao;
-    }
-
-    public void setInicioAvaliacao(Date inicioAvaliacao) {
-        this.inicioAvaliacao = inicioAvaliacao;
-    }
-
-    public Date getFimAvaliacao() {
-        return fimAvaliacao;
-    }
-
-    public void setFimAvaliacao(Date fimAvaliacao) {
-        this.fimAvaliacao = fimAvaliacao;
-    }
-
-    public Date getInicioRecurso() {
-        return inicioRecurso;
-    }
-
-    public void setInicioRecurso(Date inicioRecurso) {
-        this.inicioRecurso = inicioRecurso;
-    }
-
-    public Date getFimRecurso() {
-        return fimRecurso;
-    }
-
-    public void setFimRecurso(Date fimRecurso) {
-        this.fimRecurso = fimRecurso;
-    }
-
-    public Time getHoraFimSubmissao() {
-        return horaFimSubmissao;
-    }
-
-    public void setHoraFimSubmissao(Time horaFimSubmissao) {
-        this.horaFimSubmissao = horaFimSubmissao;
-    }
-
-    public Time getHoraInicioAvaliacao() {
-        return horaInicioAvaliacao;
-    }
-
-    public void setHoraInicioAvaliacao(Time horaInicioAvaliacao) {
-        this.horaInicioAvaliacao = horaInicioAvaliacao;
-    }
-
-    public Time getHoraFimAvaliacao() {
-        return horaFimAvaliacao;
-    }
-
-    public void setHoraFimAvaliacao(Time horaFimAvaliacao) {
-        this.horaFimAvaliacao = horaFimAvaliacao;
-    }
-
-    public Time getHoraInicioRecurso() {
-        return horaInicioRecurso;
-    }
-
-    public void setHoraInicioRecurso(Time horaInicioRecurso) {
-        this.horaInicioRecurso = horaInicioRecurso;
-    }
-
-    public Time getHoraFimRecurso() {
-        return horaFimRecurso;
-    }
-
-    public void setHoraFimRecurso(Time horaFimRecurso) {
-        this.horaFimRecurso = horaFimRecurso;
-    }
-
-    
-
-    public String getNome() {   //V
+    public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
-        //Aqui deve se verificar se "nome" já não esta atribuido a outro evento
-        //Mas presisa da implementação da camada DAO
         this.nome = nome;
     }
 
-    public List<String> getAreaConhecimento() {    //V
-        return areaConhecimento;
+    public String getDescricao() {
+        return descricao;
     }
 
-    public void setAreaConhecimento(List<String> areaConhecimento) {  //V
-        this.areaConhecimento= areaConhecimento;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
-
-    public Date getInicioSubmissao() {
-        return inicioSubmissao;
-    }
-    
-
-    public void setHoraInicioSubmissao(Time horaInicioSubmissao) {
-        this.horaInicioSubmissao = horaInicioSubmissao;
-    }
-
-    public Date getFimSubmissao() {
-        return fimSubmissao;
-    }
-
-    public void setFimSubmissao(Date fimSubmissao) {
-        this.fimSubmissao = fimSubmissao;
-    }
-
-    public void setInicioSubmissao(Date inicioSubmissao) {
-        this.inicioSubmissao = inicioSubmissao;
-    }
-
-    public Time getHoraInicioSubmissao() {
-        return horaInicioSubmissao;
-    }
-
-
-
-   
 
     public String getInstituicao() {
         return instituicao;
     }
 
-    public void setInstituicao(String instituicao) {    //OK?
+    public void setInstituicao(String instituicao) {
         this.instituicao = instituicao;
     }
 
-    public ParticipacaoList getParticipacoes() {
-        return participacoes;
+    public double getNotaDeAprovacao() {
+        return notaDeAprovacao;
     }
 
-    public void setParticipacoes(ParticipacaoList participacoes) {
-        this.participacoes = participacoes;
+    public void setNotaDeAprovacao(double notaDeAprovacao) {
+        this.notaDeAprovacao = notaDeAprovacao;
     }
-    
+
+    public Calendar getInicioSubimicao() {
+        return inicioSubimicao;
+    }
+
+    public void setInicioSubimicao(Calendar inicioSubimicao) {
+        this.inicioSubimicao = inicioSubimicao;
+    }
+
+    public Calendar getFimSubmicao() {
+        return fimSubmicao;
+    }
+
+    public void setFimSubmicao(Calendar fimSubmicao) {
+        this.fimSubmicao = fimSubmicao;
+    }
+
+    public Calendar getInicioAvaliacao() {
+        return inicioAvaliacao;
+    }
+
+    public void setInicioAvaliacao(Calendar inicioAvaliacao) {
+        this.inicioAvaliacao = inicioAvaliacao;
+    }
+
+    public Calendar getFimAvaliacao() {
+        return fimAvaliacao;
+    }
+
+    public void setFimAvaliacao(Calendar fimAvaliacao) {
+        this.fimAvaliacao = fimAvaliacao;
+    }
+
+    public Calendar getInicioRecurso() {
+        return inicioRecurso;
+    }
+
+    public void setInicioRecurso(Calendar inicioRecurso) {
+        this.inicioRecurso = inicioRecurso;
+    }
+
+    public Calendar getFimRecurso() {
+        return fimRecurso;
+    }
+
+    public void setFimRecurso(Calendar fimRecurso) {
+        this.fimRecurso = fimRecurso;
+    }
+
+    public Usuario getOrganizador() {
+        return organizador;
+    }
+
+    public void setOrganizador(Usuario organizador) {
+        this.organizador = organizador;
+    }
+
+    public UsuarioList getAvaliadores() {
+        return avaliadores;
+    }
+
+    public void setAvaliadores(UsuarioList avaliadores) {
+        this.avaliadores = avaliadores;
+    }
+
     public Arquivo getModeloDocumento() {
         return modeloDocumento;
     }
@@ -216,6 +141,15 @@ public class Evento {
     public void setModeloDocumento(Arquivo modeloDocumento) {
         this.modeloDocumento = modeloDocumento;
     }
+
+    public CompetenciaList getCompetencias() {
+        return competencias;
+    }
+
+    public void setCompetencias(CompetenciaList competencias) {
+        this.competencias = competencias;
+    }
     
     
-}
+}   
+
